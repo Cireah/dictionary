a79299201b04634c84403b58ea5407c7748b1e16
simpan824 <simon.dagernas@elev.ga.ntig.se>
2022-09-22 13:19:42 +0200

    aaaaa
    i have no idea what im doing

--- dictionary/src/Main.cpp
+++ dictionary/src/Main.cpp
@@ -1,1 +1,30 @@
-/* omong us */
+/*
+Skapa en struct som heter pair. Skapa en nyckel och en datatyp i structen. Skapa en klass som heter map.
+Använd denna klass för att spara värden i structen. Låta användaren lägga till ett nytt par i structen genom att
+använda cin. Låta användaren ta bort ett par genom att döpa om paret till något som signalerar att det är tomt.
+Låta användaren rensa hela structen genom att döpa om allt till namnet som signalerar att det är tomt. 
+Låta användaren söka på ett specifikt värde genom att skriva in en nyckel och vice versa genom att använda foreach
+
+im waaay too stupid for this
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct car {
+	string key;
+	string value;
+};
+
+class storage
+{
+	string empty = "empty";
+	int structnmbr = 0;
+};
+
+int main() {
+	return 0;
+}
